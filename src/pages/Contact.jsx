aec62a35fa820e9aee8b2f1c7751eb36b3549997
Kat Connolly <hi.connkat@gmail.com>
2022-01-25 21:49:23 -0800

    commented unused code

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,11 @@
 import "./Contact.css"
-import { useForm } from "react-hook-form";
+// import { useForm } from "react-hook-form";
 
 function Contact() {
-  const { register, handleSubmit, watch, errors } = useForm();
-  const onSubmit = (data) => console.log(data);
-  const message = watch("message") || "";
-  const messageCharsLeft = 1500 - message.length;
+  // const { register, handleSubmit, watch, errors } = useForm();
+  // const onSubmit = (data) => console.log(data);
+  // const message = watch("message") || "";
+  // const messageCharsLeft = 1500 - message.length;
 
   return (
     <div className="Contact">
@@ -15,7 +15,7 @@ function Contact() {
         <input type="email" name="user_email" placeholder="Email" />
         <br />
         <textarea name="message" placeholder="Message" maxLength="1500" />
-        <p className="message-chars-left">{messageCharsLeft}</p>
+        {/* <p className="message-chars-left">{messageCharsLeft}</p> */}
         <br />
         <input type="submit" value="Send" />
       </form>
