951127b5123efd42ab18ec6bb3329df2e91de196
Kat Connolly <hi.connkat@gmail.com>
2022-01-31 22:28:47 -0800

    added functional form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,23 +1,81 @@
-import "./Contact.css"
-// import { useForm } from "react-hook-form";
+import "./Contact.css";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+
+import { init, sendForm } from "emailjs-com";
+init("user_AmUpjUDLPeSmXgW4Qa9xC");
+
+const initialState = {
+  user_name: "",
+  user_email: "",
+  message: "",
+};
 
 function Contact() {
-  // const { register, handleSubmit, watch, errors } = useForm();
-  // const onSubmit = (data) => console.log(data);
-  // const message = watch("message") || "";
-  // const messageCharsLeft = 1500 - message.length;
+  const { register, formState } = useForm();
+  const [{ user_name, user_email, message }, setState] = useState(initialState);
+
+  const clearState = () => {
+    setState({ ...initialState });
+    console.log("sent", message);
+  };
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setState((prev) => ({ ...prev, [name]: value }));
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    sendForm("default_service", "template_qheiyl2", ".contact-form").then(
+      function (response) {
+        clearState()
+        console.log("SUCCESS!", response.status, response.text);
+      },
+      function (error) {
+        console.log("FAILED...", error);
+      }
+    );
+  };
+
+  const messageCharsLeft = 1500 - message.length;
 
   return (
     <div className="Contact">
-      <form id="contact-form">
-        <input type="text" name="user_name" placeholder="Name" />
+      <form className="contact-form" onSubmit={onSubmit}>
+        {formState.isSubmitted && (
+          <div className="success">Form submitted successfully</div>
+        )}
+        <input
+          type="text"
+          name="user_name"
+          {...register("user_name")}
+          placeholder="Name"
+          onChange={onChange}
+          value={user_name}
+        />
+
         <br />
-        <input type="email" name="user_email" placeholder="Email" />
+        <input
+          type="user_email"
+          name="user_email"
+          {...register("user_email", { required: true, maxLength: 100 })}
+          placeholder="Email"
+          onChange={onChange}
+          value={user_email}
+        />
         <br />
-        <textarea name="message" placeholder="Message" maxLength="1500" />
-        {/* <p className="message-chars-left">{messageCharsLeft}</p> */}
+        <textarea
+          name="message"
+          {...register("message", { required: true, maxLength: 1000 })}
+          placeholder="Message"
+          maxLength="1000"
+          aria-multiline={true}
+          onChange={onChange}
+          value={message}
+        />
+        <p className="message-chars-left">{messageCharsLeft}</p>
         <br />
-        <input type="submit" value="Send" />
+        <input type="submit" />
       </form>
     </div>
   );
